7b282fcb28d9fcf43d8376e5415196bc8df460ae
Leon <daleroy1@users.noreply.github.com>
2015-06-29 13:16:51 +0800

    Update widget.ragIndicator.js
    Added flashing and dim options to the lights

--- widget.ragIndicator.js
+++ widget.ragIndicator.js
@@ -6,7 +6,24 @@
 	freeboard.addStyle('.rag-light.amber', "background-color:#E49B00;box-shadow: 0px 0px 15px #E49B00;border-color:#FDF1DF;");
 	freeboard.addStyle('.rag-light.green', "background-color:#00B60E;box-shadow: 0px 0px 15px #00B60E;border-color:#FDF1DF;");
 	freeboard.addStyle('.rag-text', "margin-top:10px;");
-    
+    //Flashing status for the light
+	freeboard.addStyle('.red-flash', "animation: red-flash 1000ms infinite;")
+	freeboard.addStyle('.amber-flash', "animation: amber-flash 1000ms infinite;")
+	freeboard.addStyle('.green-flash', "animation: green-flash 1000ms infinite;")
+	//Dim status for the light
+	freeboard.addStyle('.dim', "opacity: 0.6;");
+	
+	//Keyframes for the css flashing
+	//Unless we use a library for adding keyframes to the DOM, this hack will have to suffice
+	var greenRule = "@keyframes green-flash {0% {background-color: #2A2A2A; box-shadow: 0px 0px 15px #2A2A2A;} 100% {background-color: #00B60E; box-shadow: 0px 0px 15px #00B60E;}}";
+	var amberRule = "@keyframes amber-flash {0% {background-color: #2A2A2A; box-shadow: 0px 0px 15px #2A2A2A;} 100% {background-color: #E49B00; box-shadow: 0px 0px 15px #E49B00;}}";
+	var redRule   = "@keyframes red-flash   {0% {background-color: #2A2A2A; box-shadow: 0px 0px 15px #2A2A2A;} 100% {background-color: #D90000; box-shadow: 0px 0px 15px #D90000;}}";
+	
+	//Add them to our CSS
+	var style = document.createElement('style');
+    style.innerHTML = greenRule + amberRule + redRule;
+    document.head.appendChild(style);     
+	
 	var ragWidget = function (settings) {
         var self = this;
         var titleElement = $('<h2 class="section-title"></h2>');
@@ -16,31 +33,38 @@
 		
 		//store our calculated values in an object
 		var stateObject = {};
+		
+		//array of our values: 0=Green, 2=Amber, 3=Red
+		var stateArray = ["green", "amber", "red"];
         
 		function updateState() {            
 			//Remove all classes from our indicator light
 			indicatorElement
 				.removeClass('red')
 				.removeClass('amber')					
-				.removeClass('green')		
+				.removeClass('green')	
+				.removeClass('green-flash')
+				.removeClass('amber-flash')
+				.removeClass('red-flash')
+				.removeClass('dim')
 			
-			//check our calculated indicator value and add the relevant class and calculated text
-			switch (stateObject.value) {
-				case 0:			
-					indicatorElement.addClass('green');
-					stateElement.html((_.isUndefined(stateObject.green_text) ? "" : stateObject.green_text));
+			indicatorElement.addClass(stateArray[stateObject.value]);
+			var indicatorText = stateArray[stateObject.value] + '_text';
+			//Get our Indicator Type
+			stateElement.html((_.isUndefined(stateObject[indicatorText]) ? "" : stateObject[indicatorText]));
+			var indicatorType = (_.isUndefined(stateObject.indicator_type) ? "" : stateObject.indicator_type.toLowerCase());
+			
+			switch (indicatorType) {
+				case 'dim' : 
+					indicatorElement.addClass('dim');
 					break;
-				case 1:			
-					indicatorElement.addClass('amber');
-					stateElement.html((_.isUndefined(stateObject.amber_text) ? "" : stateObject.amber_text));
-					break;
-				case 2:			
-					indicatorElement.addClass('red');
-					stateElement.html((_.isUndefined(stateObject.red_text) ? "" : stateObject.red_text));
+				case 'flash' :
+					var indicatorTypeClass = stateArray[stateObject.value] + '-flash';
+					indicatorElement.addClass(indicatorTypeClass);				
 					break;
 				default:
-					stateElement.html("Error retrieving status");
-				}
+					//this is normal					
+			}			
         }
 
         this.render = function (element) {
@@ -56,7 +80,7 @@
         }
 
         this.onCalculatedValueChanged = function (settingName, newValue) {
-            //whenever a calculated value changes, stored them in the variable 'stateObject'
+            //whenever a calculated value changes, store them in the variable 'stateObject'
 			stateObject[settingName] = newValue;
             updateState();
         }
@@ -98,6 +122,11 @@
 			{
                 name: "red_text",
                 display_name: "Red Text",
+                type: "calculated"
+            },
+			{
+                name: "indicator_type",
+                display_name: "Type (Normal, Dim, Flash)",
                 type: "calculated"
             }
             
